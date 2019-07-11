1316bc43e27f833168a11a93a28932c5d2c02794
GORDON YANG (ITA-ISDC-ISD-OOCLL/ZHA) <YANGGO@OOCL.COM>
2019-07-11 11:34:53 +0800

    update

--- practices/map/map_to_four_multiples_add_one.js
+++ practices/map/map_to_four_multiples_add_one.js
@@ -1,6 +1,9 @@
 'use strict';
+const mapToFourMultiplesAdd = number =>{
+  return number*4+1;
+}
 var map_to_four_multiples_add_one = function(collection){
-  return [5,9,13,17,21];
+  return collection.map(mapToFourMultiplesAdd);
 };
 
 module.exports = map_to_four_multiples_add_one;
